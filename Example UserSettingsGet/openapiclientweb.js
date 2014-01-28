a937614705c979f26825a68470cc894fc8b70f7c
ambenpau <pauinho_FCB@hotmail.com>
2014-01-28 13:17:55 +0100

    UserSettings i UserTutors
    He afegit els exemples de UserSettings i UserTutors

---
+++ Example UserSettingsGet/openapiclientweb.js
@@ -0,0 +1,87 @@
+$(function() {
+	var extractToken = function(hash) {
+			var match = hash.match(/access_token=([\w-]+)/);
+			return !!match && match[1];
+	};
+
+	var setting = {
+		'host': "http://oslo.uoc.es:8080/webapps/uocapi",
+		'clientId': "EUI3WhtdZMjwOLxwFyzUcLUUXeE0ACmB6See1HWRjoCSIgGJ1If8EI8EPorzSWFRRnw1fxkOvIqkBI1d91GGKKG6nFnD35OY6VOEx4LcFLQIW3Z2jGsTaYey4bUcbR4K"
+	};
+	
+	var authorizeHost = setting.host + "/oauth/authorize";
+	var apiHost = setting.host + "/api/v1";
+
+	var getCode = function() {
+		var authUrl = authorizeHost + 
+			"?response_type=token" + 
+			"&scope=READ READ_MAIL READ_BOARD SEND_MAIL SEND_BOARD WRITE" + 
+			"&client_id=" + setting.clientId + 
+			"&state=estado" + 
+			"&redirect_uri=http://" + window.location.host + window.location.pathname;
+
+		$('a.connect').attr("href", authUrl);
+		$('.authenticate').show();
+	};
+/*STARTUOCAPIEXAMPLE*/
+	var getData = function(token) {
+		$('span.token').text(token);
+		$('div.authenticated').show();
+		
+		$.ajax({
+			/* UOCAPICALL /api/v1/user/settings GET*/
+			url: apiHost + '/user/settings?access_token='+token,
+			type: "GET",
+			dataType: "json",
+			success: function(response) {
+				
+				if (response) {
+					var iter = 0;
+					var table = document.getElementById("userSettings");
+					table.innerHTML = "";
+					var firstrow = table.insertRow();
+					for(iter; iter < 7; iter++) {
+					  firstrow.insertCell();
+					}
+					firstrow.cells[0].innerHTML = "<b>id</b>";
+					firstrow.cells[1].innerHTML = "<b>title</b>";
+					firstrow.cells[2].innerHTML = "<b>description</b>";
+					firstrow.cells[3].innerHTML = "<b>section</b>";
+					firstrow.cells[4].innerHTML = "<b>url</b>";
+					firstrow.cells[5].innerHTML = "<b>value</b>";
+					firstrow.cells[6].innerHTML = "<b>values</b>";
+					iter = 0;
+					var i = 0;
+					while (iter < response.settings.length){
+						var row = table.insertRow(-1);
+						for(i; i < 7; i++) {
+							row.insertCell();
+						}
+						i = 0;
+						row.cells[0].innerHTML = response.settings[iter].id;
+						row.cells[1].innerHTML = response.settings[iter].title;
+						row.cells[2].innerHTML = response.settings[iter].description;
+						row.cells[3].innerHTML = response.settings[iter].section;
+						row.cells[4].innerHTML = response.settings[iter].url;
+						row.cells[5].innerHTML = response.settings[iter].value;
+						row.cells[6].innerHTML = response.settings[iter].values;// Aquest s'ha de millorar.
+						iter = iter + 1;
+					}
+				}
+			}
+		});
+	};
+/*ENDUOCAPIEXAMPLE*/
+	var token = extractToken(document.location.hash);
+	if (token) {
+		// put access token as a request header for all AJAX requests
+		$.ajaxSetup({
+			headers: {
+				"Authorization": "Bearer " + token
+			}
+		});
+		getData(token);
+	} else {
+		getCode();
+	};
+});
